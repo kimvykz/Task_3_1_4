c3ae29d10094ca8fc487cdaedba6c427a1c39edb
kimvykz <kimslavakz@gmail.com>
2023-08-27 00:33:23 +0900

    v1.2 - fixed N+1 problem with "allUsers" method

--- src/main/java/kr/habsida/Task_3_1_4/repository/UserRepository.java
+++ src/main/java/kr/habsida/Task_3_1_4/repository/UserRepository.java
@@ -1,13 +1,21 @@
 package kr.habsida.Task_3_1_4.repository;
 
 import kr.habsida.Task_3_1_4.model.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @EntityGraph(attributePaths = "roles")
+    @Query("select a from User a")
+    List<User> findAll();
+
     Optional<User> findByEmail(String email);
     Boolean existsByEmail(String email);
 }
